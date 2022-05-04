5e7ba32c713fe3e793458bafad7beb1f5454f9bc
L. Dolio Durant <dolio@zipcodewilmington.com>
2022-05-04 12:28:27 -0400

    fixing test to be mathematically valid

--- src/test/java/rocks/zipcode/io/quiz4/generics/genericarrayutils/powerset/PowerSetOfThreeElements.java
+++ src/test/java/rocks/zipcode/io/quiz4/generics/genericarrayutils/powerset/PowerSetOfThreeElements.java
@@ -10,7 +10,7 @@ public class PowerSetOfThreeElements {
     public void test1() {
         // given
         String[] input = {"a", "b", "c"};
-        String expectedString = "[[a, b, c], [a, b], [a, c], [a], [b, a], [b, c], [b], [c, a], [c, b], [c], []]";
+        String expectedString = "[[a, b, c], [a, b], [a, c], [a], [b, c], [b], [c], []]";
         PowerSetTest.test(input, expectedString);
     }
 
@@ -18,7 +18,7 @@ public class PowerSetOfThreeElements {
     public void test2() {
         // given
         String[] input = {"1", "2", "3"};
-        String expectedString = "[[1, 2, 3], [1, 2], [1, 3], [1], [2, 1], [2, 3], [2], [3, 1], [3, 2], [3], []]";
+        String expectedString = "[[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []]";
         PowerSetTest.test(input, expectedString);
     }
 
@@ -26,7 +26,7 @@ public class PowerSetOfThreeElements {
     public void test3() {
         // given
         String[] input = {"x", "y", "z"};
-        String expectedString = "[[x, y, z], [x, y], [x, z], [x], [y, x], [y, z], [y], [z, x], [z, y], [z], []]";
+        String expectedString = "[[x, y, z], [x, y], [x, z], [x], [y, z], [y], [z], []]";
         PowerSetTest.test(input, expectedString);
     }
 
@@ -34,7 +34,7 @@ public class PowerSetOfThreeElements {
     public void test4() {
         // given
         String[] input = {"aFIRST", "bSECOND", "cTHIRD"};
-        String eaFIRSTpectedString = "[[aFIRST, bSECOND, cTHIRD], [aFIRST, bSECOND], [aFIRST, cTHIRD], [aFIRST], [bSECOND, aFIRST], [bSECOND, cTHIRD], [bSECOND], [cTHIRD, aFIRST], [cTHIRD, bSECOND], [cTHIRD], []]";
+        String eaFIRSTpectedString = "[[aFIRST, bSECOND, cTHIRD], [aFIRST, bSECOND], [aFIRST, cTHIRD], [aFIRST], [bSECOND, cTHIRD], [bSECOND], [cTHIRD], []]";
         PowerSetTest.test(input, eaFIRSTpectedString);
     }
 
@@ -43,7 +43,7 @@ public class PowerSetOfThreeElements {
     public void test5() {
         // given
         String[] input = {"1WATER", "2FIRE", "3LIGHTNING"};
-        String e1WATERpectedString = "[[1WATER, 2FIRE, 3LIGHTNING], [1WATER, 2FIRE], [1WATER, 3LIGHTNING], [1WATER], [2FIRE, 1WATER], [2FIRE, 3LIGHTNING], [2FIRE], [3LIGHTNING, 1WATER], [3LIGHTNING, 2FIRE], [3LIGHTNING], []]";
+        String e1WATERpectedString = "[[1WATER, 2FIRE, 3LIGHTNING], [1WATER, 2FIRE], [1WATER, 3LIGHTNING], [1WATER], [2FIRE, 3LIGHTNING], [2FIRE], [3LIGHTNING], []]";
         PowerSetTest.test(input, e1WATERpectedString);
     }
 }
